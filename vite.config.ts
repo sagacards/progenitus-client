2972cd9beb64c49e95d4fd14a8a37cf416d4588a
jorgenbuilder <jorgen@buildnode.io>
2022-03-16 11:16:55 -0700

    provision project

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    define: {
+        global: 'window',
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true,
+                })
+            ]
+        }
+    }
+})
