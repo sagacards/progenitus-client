0071d6f0663ffbb81a8ccd37cb8949dbed7463c5
jorgenbuilder <jorgen@buildnode.io>
2022-03-16 16:40:38 -0700

    feat: scaffold dark/light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,13 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import useStore from 'stores/index';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    const { colorScheme, setColorScheme } = useStore();
+    return <div>
+        <h1>Hello World!</h1>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <a href="#" onClick={() => setColorScheme(colorScheme === 'dark' ? 'light': 'dark')}>Change Color Scheme</a>
     </div>
-  )
 }
 
 export default App
---
+++ src/stores/index.ts
@@ -0,0 +1,45 @@
+import create from 'zustand'
+
+type ColorScheme = 'dark' | 'light';
+
+interface Store {
+    colorScheme: ColorScheme;
+    setColorScheme: (c : ColorScheme) => void;
+};
+
+function getUserColorScheme () : ColorScheme {
+    let scheme : ColorScheme = 'dark';
+
+    const savedValue = window.localStorage.getItem('prefers-color-scheme');
+    const sysPreferDark : boolean = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const sysPreferLight : boolean = window.matchMedia('(prefers-color-scheme: light)').matches;
+
+    if (savedValue === 'dark') {
+        scheme = 'dark';
+    } else if (savedValue === 'light') {
+        scheme = 'light';
+    } else if (sysPreferDark) {
+        scheme = 'dark';
+    } else if (sysPreferLight) {
+        scheme = 'light';
+    }
+
+    document.querySelector('html')?.setAttribute('data-theme', scheme);
+
+    return scheme;
+}
+
+const useStore = create<Store>((set, get) => ({
+
+    // Dark/Light Color Scheme
+
+    colorScheme: getUserColorScheme(),
+    setColorScheme (colorScheme) {
+        set({ colorScheme });
+        window.localStorage.setItem('prefers-color-scheme', colorScheme);
+        document.querySelector('html')?.setAttribute('data-theme', colorScheme);
+    },
+
+})) 
+
+export default useStore;
