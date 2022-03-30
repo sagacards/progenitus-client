5242feeb0450efe7f0c4de152fc4a635088b7cdb
jorgenbuilder <jorgen@buildnode.io>
2022-03-30 12:45:35 -0700

    palceholder art

--- src/ui/drop-card/list.tsx
+++ src/ui/drop-card/list.tsx
@@ -23,6 +23,6 @@ export default function DropCardList (props : Props) {
         art: 'https://i.imgur.com/8cOXOCE.mp4',
     },]
     return <div className={Styles.list}>
-        {Object.values(events).map((drop, i) => <DropCard key={`drop${i}`} name={drop.collection.name} id={drop.id} art={''} />)}
+        {Object.values(events).map((drop, i) => <DropCard key={`drop${i}`} name={drop.collection.name} id={drop.id} art={drops[i].art} />)}
     </div>
 }
