2d0dcd8e486422e6d038a1c8476c20382aa87e95
Jorgen <jorgen@buildnode.io>
2022-04-05 13:21:30 -0700

    chore: update canister interface

--- src/canisters/progenitus/progenitus.did.d.ts
+++ src/canisters/progenitus/progenitus.did.d.ts
@@ -1,19 +1,50 @@
 import type { Principal } from '@dfinity/principal';
+export type Access = { 'Private' : StableAllowlist } |
+  { 'Public' : null };
 export type AccountIdentifier = Array<number>;
 export type BlockIndex = bigint;
+export interface CollectionDetails {
+  'descriptionMarkdownUrl' : string,
+  'iconImageUrl' : string,
+  'bannerImageUrl' : string,
+  'previewImageUrl' : string,
+}
+export interface Data {
+  'startsAt' : Time,
+  'name' : EventName,
+  'description' : string,
+  'accessType' : Access,
+  'details' : CollectionDetails,
+  'price' : Tokens,
+  'endsAt' : Time,
+}
+export type EventName = string;
+export type Events = Array<[Principal, Data]>;
 export interface Rex {
   'addAdmin' : (arg_0: Principal) => Promise<undefined>,
   'balance' : () => Promise<Tokens>,
+  'createEvent' : (arg_0: Data) => Promise<bigint>,
   'getAdmins' : () => Promise<Array<Principal>>,
+  'getAllEvents' : () => Promise<Events>,
+  'getAllowlistSpots' : (arg_0: Principal, arg_1: bigint) => Promise<
+      [] | [bigint]
+    >,
+  'getEvent' : (arg_0: Principal, arg_1: bigint) => Promise<[] | [Data]>,
+  'getEvents' : (arg_0: Array<Principal>) => Promise<Events>,
+  'getEventsOfToken' : (arg_0: Principal) => Promise<Array<Data>>,
+  'getOwnEvents' : () => Promise<Array<Data>>,
   'getPersonalAccount' : () => Promise<AccountIdentifier>,
   'getPrice' : () => Promise<Tokens>,
+  'mint' : (arg_0: Principal, arg_1: bigint) => Promise<bigint>,
   'removeAdmin' : (arg_0: Principal) => Promise<undefined>,
   'setPrice' : (arg_0: Tokens) => Promise<undefined>,
-  'syncAvailableTokens' : () => Promise<undefined>,
   'transfer' : (arg_0: Tokens, arg_1: AccountIdentifier) => Promise<
       TransferResult
     >,
+  'updateEvent' : (arg_0: bigint, arg_1: Data) => Promise<undefined>,
 }
+export type StableAllowlist = Array<[Principal, [] | [bigint]]>;
+export type Time = bigint;
 export interface Tokens { 'e8s' : bigint }
 export type TransferError = {
     'TxTooOld' : { 'allowed_window_nanos' : bigint }
--- src/canisters/progenitus/progenitus.did.js
+++ src/canisters/progenitus/progenitus.did.js
@@ -1,5 +1,28 @@
 export const idlFactory = ({ IDL }) => {
   const Tokens = IDL.Record({ 'e8s' : IDL.Nat64 });
+  const Time = IDL.Int;
+  const EventName = IDL.Text;
+  const StableAllowlist = IDL.Vec(IDL.Tuple(IDL.Principal, IDL.Opt(IDL.Int)));
+  const Access = IDL.Variant({
+    'Private' : StableAllowlist,
+    'Public' : IDL.Null,
+  });
+  const CollectionDetails = IDL.Record({
+    'descriptionMarkdownUrl' : IDL.Text,
+    'iconImageUrl' : IDL.Text,
+    'bannerImageUrl' : IDL.Text,
+    'previewImageUrl' : IDL.Text,
+  });
+  const Data = IDL.Record({
+    'startsAt' : Time,
+    'name' : EventName,
+    'description' : IDL.Text,
+    'accessType' : Access,
+    'details' : CollectionDetails,
+    'price' : Tokens,
+    'endsAt' : Time,
+  });
+  const Events = IDL.Vec(IDL.Tuple(IDL.Principal, Data));
   const AccountIdentifier = IDL.Vec(IDL.Nat8);
   const BlockIndex = IDL.Nat64;
   const TransferError = IDL.Variant({
@@ -16,14 +39,26 @@ export const idlFactory = ({ IDL }) => {
   const Rex = IDL.Service({
     'addAdmin' : IDL.Func([IDL.Principal], [], ['oneway']),
     'balance' : IDL.Func([], [Tokens], []),
+    'createEvent' : IDL.Func([Data], [IDL.Nat], []),
     'getAdmins' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'getAllEvents' : IDL.Func([], [Events], ['query']),
+    'getAllowlistSpots' : IDL.Func(
+        [IDL.Principal, IDL.Nat],
+        [IDL.Opt(IDL.Int)],
+        ['query'],
+      ),
+    'getEvent' : IDL.Func([IDL.Principal, IDL.Nat], [IDL.Opt(Data)], ['query']),
+    'getEvents' : IDL.Func([IDL.Vec(IDL.Principal)], [Events], ['query']),
+    'getEventsOfToken' : IDL.Func([IDL.Principal], [IDL.Vec(Data)], ['query']),
+    'getOwnEvents' : IDL.Func([], [IDL.Vec(Data)], ['query']),
     'getPersonalAccount' : IDL.Func([], [AccountIdentifier], ['query']),
     'getPrice' : IDL.Func([], [Tokens], ['query']),
+    'mint' : IDL.Func([IDL.Principal, IDL.Nat], [IDL.Nat], []),
     'removeAdmin' : IDL.Func([IDL.Principal], [], ['oneway']),
     'setPrice' : IDL.Func([Tokens], [], ['oneway']),
-    'syncAvailableTokens' : IDL.Func([], [], ['oneway']),
     'transfer' : IDL.Func([Tokens, AccountIdentifier], [TransferResult], []),
+    'updateEvent' : IDL.Func([IDL.Nat, Data], [], []),
   });
   return Rex;
 };
-export const init = ({ IDL }) => { return [IDL.Text, IDL.Text]; };
+export const init = ({ IDL }) => { return [IDL.Text]; };
