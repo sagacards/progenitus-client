d150f3e254b5aa718941ea65b4c62fc450db9769
Jorgen <jorgen@buildnode.io>
2022-04-06 13:48:31 -0700

    feat: creator image

--- src/ui/lineage/index.tsx
+++ src/ui/lineage/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hashatar from 'ui/hashatar';
 import Styles from './styles.module.css'
 
+import Saga from 'assets/disk/8.png'
+
 interface Props {
     children?: React.ReactNode;
 }
@@ -9,20 +11,20 @@ interface Props {
 export default function Lineage (props : Props) {
     return <div className={Styles.root}>
         <div className={Styles.item}>
-            <div className={Styles.disk}><Hashatar name="creator" /></div>
-            <div className={Styles.tip}>Created By</div>
+            <div className={Styles.disk}><img className={Styles.image} src={Saga} /></div>
+            <div className={Styles.tip}>Creator: Saga Cards</div>
         </div>
         <div className={Styles.item}>
             <div className={Styles.disk}><Hashatar name="collection" /></div>
-            <div className={Styles.tip}>Collection</div>
+            <div className={Styles.tip}>Collection: </div>
         </div>
         <div className={Styles.item}>
             <div className={Styles.disk}><Hashatar name="minter" /></div>
-            <div className={Styles.tip}>Minted By</div>
+            <div className={Styles.tip}>Minter: ?</div>
         </div>
         <div className={Styles.item}>
             <div className={Styles.disk}><Hashatar name="owner" /></div>
-            <div className={Styles.tip}>Current Owner</div>
+            <div className={Styles.tip}>Owner: ?</div>
         </div>
     </div>
 }
