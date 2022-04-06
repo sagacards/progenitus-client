b99306a2a4ddda121ffcf7b4db6f1e0febb57c5b
Jorgen <jorgen@buildnode.io>
2022-04-06 10:21:19 -0700

    feat: deep link scrolling

--- src/ui/scroll-to-top/index.tsx
+++ src/ui/scroll-to-top/index.tsx
@@ -4,7 +4,12 @@ import { useLocation } from 'react-router-dom'
 export default function ScrollToTop () {
     const location = useLocation();
     React.useEffect(() => {
-        window.scrollTo(0, 0);
+        if(location.hash) {
+            // @ts-ignore: offsetTop actually does exist
+            window.scrollTo(0, (document.querySelector(location.hash)?.offsetTop || 0) - 78);
+        } else {
+            window.scrollTo(0, 0);
+        }
     }, [location]);
     return <></>
 };
--- src/ui/tabs/index.tsx
+++ src/ui/tabs/index.tsx
@@ -11,7 +11,7 @@ export default function Tabs ({ tabs } : Props) {
     const index = React.useMemo(() => Math.max(tabs.findIndex(tab => tab[0].toLowerCase() === hash.toLowerCase()), 0), [hash, tabs]);
     return <div className={Styles.root}>
         <div className={Styles.tabs}>
-            {tabs.map((tab, i) => <div className={[Styles.tab, index === i ? Styles.active : ''].join(' ')} key={`tab${tab[0]}`} onClick={() => { setHash(tab[0].toLowerCase()); window.location.hash = tab[0].toLowerCase() }}>
+            {tabs.map((tab, i) => <div id={tab[0].toLowerCase()} className={[Styles.tab, index === i ? Styles.active : ''].join(' ')} key={`tab${tab[0]}`} onClick={() => { setHash(tab[0].toLowerCase()); window.location.hash = tab[0].toLowerCase() }}>
                 {tab[0]}
             </div>)}
         </div>
